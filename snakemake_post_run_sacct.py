63ce116068ceb47b93188832f1b2cc39f8783012
SteampunkIslande <charles.monod-broca@neuf.fr>
2026-02-23 17:49:31 +0100

    Added post run sacct call

--- snakemake_post_run_sacct.py
+++ snakemake_post_run_sacct.py
@@ -6,6 +6,8 @@
 import subprocess
 import os
 import re
+from pathlib import Path
+
 
 if __name__ == "__main__":
     import argparse
@@ -14,13 +16,16 @@ if __name__ == "__main__":
         description="Post-run script for Snakemake to generate a Slurm usage report."
     )
     parser.add_argument(
-        "workflow_dir", help="Path to the Snakemake workflow directory."
+        "workflow_dir",
+        help="Path to the Snakemake workflow directory.",
     )
     args = parser.parse_args()
 
-    expected_log_dir = os.path.join(args.workflow_dir, ".snakemake", "log")
+    workflow_dir = Path(args.workflow_dir).resolve()
 
-    if not os.path.isdir(expected_log_dir):
+    expected_log_dir = workflow_dir / ".snakemake" / "log"
+
+    if not expected_log_dir.is_dir():
         print(f"Error: Expected log directory not found at {expected_log_dir}")
         exit(1)
 
@@ -28,81 +33,77 @@ if __name__ == "__main__":
         r"^(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})T(?P<hour>\d{2})(?P<minutes>\d{2})(?P<seconds>\d{2}).+\.snakemake\.log$"
     )
 
-    for filename in os.listdir(expected_log_dir):
-        m = re.match(file_name_re, filename)
+    for filename in expected_log_dir.iterdir():
+        m = re.match(file_name_re, filename.name)
         if m:
-            log_path = os.path.join(expected_log_dir, filename)
-            with open(log_path, "r") as log_file:
-
+            log_path = expected_log_dir / filename
+            with log_path.open("r") as log_file:
                 for line in log_file:
                     if "SLURM run ID: " in line:
                         job_id = line.strip()[14:]
 
-        sacct_report_path = f"sacct_report_{m.group('year')}-{m.group('month')}-{m.group('day')}T{m.group('hour')}:{m.group('minutes')}:{m.group('seconds')}.csv"
-        try:
-            with open(
-                os.path.join(
-                    args.workflow_dir,
-                    sacct_report_path,
-                ),
-                "a",
-            ) as report_file:
-                # 1: Obtenir le rapport sacct pour le job_id
-                subprocess.run(
-                    [
-                        "sacct",
-                        "--name",
-                        job_id,
-                        "-o",
-                        "ALL",
-                        "-P",
-                    ],
-                    stdout=report_file,
-                    text=True,
-                    check=True,
-                )
-                # 2: convertir
-                subprocess.run(
-                    [
-                        "singularity",
-                        "exec",
-                        "/SINGULARITIES/slurm-usage-report.sif",
-                        "python",
-                        "/app/main.py",
-                        "csv_to_parquet",
-                        "-i",
-                        os.path.join(args.workflow_dir, sacct_report_path),
-                        "-o",
-                        os.path.join(
-                            args.workflow_dir,
-                            sacct_report_path.replace(".csv", ".parquet"),
-                        ),
-                    ],
-                    text=True,
-                    check=True,
-                )
-                subprocess.run(
-                    [
-                        "singularity",
-                        "exec",
-                        "/SINGULARITIES/slurm-usage-report.sif",
-                        "python",
-                        "/app/main.py",
-                        "snakemake_efficiency",
-                        "-i",
-                        os.path.join(
-                            args.workflow_dir,
-                            sacct_report_path.replace(".csv", ".parquet"),
-                        ),
-                        "-o",
-                        os.path.join(
-                            args.workflow_dir,
-                            sacct_report_path.replace(".csv", ".html"),
-                        ),
-                    ],
-                    text=True,
-                    check=True,
-                )
+                        sacct_report_path = Path(
+                            f"sacct_report_{m.group('year')}-{m.group('month')}-{m.group('day')}T{m.group('hour')}:{m.group('minutes')}:{m.group('seconds')}.csv"
+                        )
+                        report_path_csv = workflow_dir / sacct_report_path
+                        try:
+                            with open(
+                                workflow_dir / sacct_report_path,
+                                "w",
+                            ) as report_file:
+                                # 1: Obtenir le rapport sacct pour le job_id
+                                subprocess.run(
+                                    [
+                                        "sacct",
+                                        "--name",
+                                        job_id,
+                                        "-o",
+                                        "ALL",
+                                        "-P",
+                                    ],
+                                    stdout=report_file,
+                                    text=True,
+                                    check=True,
+                                )
+                                # 2: convertir les CSV en parquet (plus rapide pour les requêtes)
+                                subprocess.run(
+                                    [
+                                        "singularity",
+                                        "exec",
+                                        "/SINGULARITIES/slurm-usage-report-1.0.0.sif",
+                                        "/app/main.py",
+                                        "csv_to_parquet",
+                                        "-i",
+                                        workflow_dir / sacct_report_path,
+                                        "-o",
+                                        workflow_dir
+                                        / sacct_report_path.with_suffix(".parquet"),
+                                    ],
+                                    text=True,
+                                    check=True,
+                                    env={**os.environ, "SINGULARITY_BIND": "/tmp"},
+                                )
+                                subprocess.run(
+                                    [
+                                        "singularity",
+                                        "exec",
+                                        "/SINGULARITIES/slurm-usage-report-1.0.0.sif",
+                                        "/app/main.py",
+                                        "snakemake_efficiency",
+                                        "-i",
+                                        workflow_dir
+                                        / sacct_report_path.with_suffix(".parquet"),
+                                        "-o",
+                                        workflow_dir
+                                        / sacct_report_path.with_suffix(".html"),
+                                    ],
+                                    text=True,
+                                    check=True,
+                                    env={**os.environ, "SINGULARITY_BIND": "/tmp"},
+                                )
 
-        except subprocess.CalledProcessError as e:
-            print(f"Error calling sacct: {e}")
+                                if report_path_csv.is_file():
+                                    report_path_csv.unlink()
+
+                        except subprocess.CalledProcessError as e:
+                            print(f"Error calling sacct: {e}")

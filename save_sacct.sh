524a551fbb8fe6a514ce18861b223c5da53ead97
SteampunkIslande <charles.monod-broca@neuf.fr>
2026-02-25 10:23:32 +0100

    Fixed save_sacct

--- save_sacct.sh
+++ save_sacct.sh
@@ -9,6 +9,6 @@ f=SACCT_DB/$yesterday.csv
 
 sacct -a -P -o 'ALL' -S "${yesterday}T00:00:00" -E "${yesterday}T23:59:59" > $f
 
-singularity run -B /tmp /SINGULARITIES/slurm-usage-report-1.0.0.sif /app/main.py csv_to_parquet -i ${f} -o ${f%.csv}.parquet
+singularity run -B /tmp /SINGULARITIES/slurm-usage-report-1.0.0.sif /app/usage-report.py csv_to_parquet -i ${f} -o ${f%.csv}.parquet
 
 rm $f
